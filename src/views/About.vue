57bc4b01f27111b9175410ebb354eacd02022d70
Julien Calixte <julienc@bam.tech>
2020-04-13 21:24:25 +0200

    :speech_balloon: (credits) add flaticon credits in about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    Icons made by
-    <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons"
-      >Smashicons</a
-    >
-    from
-    <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+    <p>
+      Icons made by
+      <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons"
+        >Smashicons</a
+      >
+      from
+      <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+    </p>
   </div>
 </template>
