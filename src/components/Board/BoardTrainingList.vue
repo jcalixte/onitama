dfa03f747b2209d8bfcec4a3aacdc1f003a7b3d8
Julien Calixte <julienc@bam.tech>
2020-04-16 16:55:28 +0200

    :sparkles: (ai) add loader on training list

--- src/components/Board/BoardTrainingList.vue
+++ src/components/Board/BoardTrainingList.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="board-training-list" v-if="boards.length">
-    <h3 class="subtitle is-3">boards played</h3>
-    <ul v-for="[date, boards] in boardByDate" :key="date">
-      <h4 class="title is-4">{{ date }}</h4>
-      <ul class="board-user-list">
-        <li v-for="board in boards" :key="board._id">
-          <BoardUserListItem :board="board" />
-        </li>
+  <div class="board-training-list">
+    <div v-if="loading" class="loading">
+      <i class="gg-loadbar-alt"></i>
+    </div>
+    <div v-else-if="!loading && boards.length">
+      <h3 class="subtitle is-3">boards played</h3>
+      <ul v-for="[date, boards] in boardByDate" :key="date">
+        <h4 class="title is-4">{{ date }}</h4>
+        <ul class="board-user-list">
+          <li v-for="board in boards" :key="board._id">
+            <BoardUserListItem :board="board" />
+          </li>
+        </ul>
+        <br />
       </ul>
-      <br />
-    </ul>
+    </div>
   </div>
 </template>
 
@@ -26,6 +31,7 @@ import { Board } from '@/models/Board'
 export default class BoardTrainingList extends Vue {
   private boardByDate: [string, Board[]][] = []
   private boards: Board[] = []
+  private loading = true
 
   public async mounted() {
     this.boards = await repository.getTrainingDocuments()
@@ -36,6 +42,16 @@ export default class BoardTrainingList extends Vue {
       boardByDate.set(date, [...(oldArray || []), board])
     })
     this.boardByDate = Array.from(boardByDate)
+    this.loading = false
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.loading {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px 0;
+}
+</style>
