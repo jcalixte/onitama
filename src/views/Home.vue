b4edf285f883db99861cb58e746a69a2f42bdbf8
Julien Calixte <julienc@bam.tech>
2020-04-26 09:47:58 +0200

    :art: (home) change bot section disposition

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,34 +6,42 @@
     <div class="buttons is-centered">
       <BoardNew />
     </div>
-    <section class="hero is-primary is-bold">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">
-            Play against Zhuge Liang?
-          </h1>
-          <!-- <img src="../assets/zhuge-liang.jpg" alt="" /> -->
-          <div class="buttons is-centered">
-            <BoardNew play-against-a-i="zhuge" />
-            <router-link class="button is-primary is-light" to="/board-training"
-              >display training</router-link
-            >
+    <div class="columns">
+      <div class="column">
+        <section class="hero is-primary is-bold">
+          <div class="hero-body">
+            <div class="container">
+              <h1 class="title">
+                Play against Zhuge Liang?
+              </h1>
+              <!-- <img src="../assets/zhuge-liang.jpg" alt="" /> -->
+              <div class="buttons is-centered">
+                <BoardNew play-against-a-i="zhuge" />
+                <router-link
+                  class="button is-primary is-light"
+                  to="/board-training"
+                  >display training</router-link
+                >
+              </div>
+            </div>
           </div>
-        </div>
+        </section>
       </div>
-    </section>
-    <section class="hero is-warning is-bold">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">
-            Play against Sima Yi?
-          </h1>
-          <div class="buttons is-centered">
-            <BoardNew play-against-a-i="sima" />
+      <div class="column">
+        <section class="hero is-warning is-bold">
+          <div class="hero-body">
+            <div class="container">
+              <h1 class="title">
+                Play against Sima Yi?
+              </h1>
+              <div class="buttons is-centered">
+                <BoardNew play-against-a-i="sima" />
+              </div>
+            </div>
           </div>
-        </div>
+        </section>
       </div>
-    </section>
+    </div>
     <BoardUserList />
   </div>
 </template>
