c37d8379ec5027d7366513595cbf750e44a2eacb
Julien Calixte <julienc@bam.tech>
2020-06-01 16:55:52 +0200

    :speech_balloon: (about / rules) update text

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="about">
-    <h1 class="title is-1">About <span class="onitama">Onitama</span> space</h1>
+    <h1 class="title is-1">About <span class="onitama">Onitama</span>.space</h1>
+    <p>
+      This web app was made by
+      <a
+        href="https://github.com/jcalixte/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Juju
+      </a>
+      with
+      <code>&lt;3</code>
+    </p>
     <p>
       Icons made by
       <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons"
--- src/views/PlayRules.vue
+++ src/views/PlayRules.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="play-rules">
     <h1 class="title is-1"><span class="onitama">Onitama</span>'s rules</h1>
-    <div class="columns is-centered">
-      <div class="column is-half">
-        <p>
-          <span class="onitama">Onitama</span> is an elegant and simple game
-          which captures the essence of martial arts.. Each game is quick,
-          usually 15 minutes in length.
-        </p>
-      </div>
-    </div>
-    <a href="http://www.arcanewonders.com/resources/Onitama_Rulebook.PDF"
-      >Official rules</a
-    >
+    <p>
+      <span class="onitama">Onitama</span> is an elegant and simple game which
+      captures the essence of martial arts.. Each game is quick, usually 15
+      minutes in length.
+    </p>
+    <p>
+      For full explanation, take a look at the
+      <a
+        href="http://www.arcanewonders.com/resources/Onitama_Rulebook.PDF"
+        target="_blank"
+        rel="noopener noreferrer"
+        >official rules</a
+      >.
+    </p>
   </div>
 </template>
 
@@ -27,7 +29,9 @@ export default class PlayRules extends Vue {}
 .play-rules {
   font-size: 1.4rem;
   p {
+    max-width: 50vw;
     text-align: justify;
+    margin: auto;
   }
 }
 </style>
